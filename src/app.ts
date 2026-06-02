d0e5ffb7602a9b8172aafc3437bd7fcc772f9364
LITA.SOK <LITA.SOK@PNC.LOCAL>
2026-06-02 16:04:10 +0700

    update code

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ class App {
 
     this.app.use('/api/auth', authRoutes);
     this.app.use('/api/teacher', teacherRoutes);
-    this.app.use('/api/student', studentRoutes);
+    this.app.use('/api/students', studentRoutes);
     this.app.use('/api/dashboard', dashboardRoutes);
     
     
